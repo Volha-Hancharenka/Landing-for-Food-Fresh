ff4830f2f97b17fbeab3c881024d70c1d51365ce
Volha-Hancharenka <goncharenkolga4567@gmail.com>
2022-11-13 19:43:21 +0300

    active

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function setburgerMenu() {
+  const burgerBtn = document.querySelector('.header__burger')
+  const header = document.querySelector('.header')
+  const menu = document.querySelector('.menu')
+  let isLock = false
+  
+
+  burgerBtn.addEventListener('click', () => {
+    isLock = !isLock
+    //toggleBodyLock(isLock)
+
+    burgerBtn.classList.toggle('active')
+    menu.classList.toggle('active')
+    header.classList.toggle('active')
+  })
+}
